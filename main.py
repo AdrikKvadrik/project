94db8ecd76752980b3127d6c37c9177786634236
AdrtikKvadrik <adrian.fadyeyev@gmail.com>
2023-05-27 21:31:47 +0300

    dz/10

--- main.py
+++ main.py
@@ -1,64 +1,85 @@
 import sqlite3
 
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('products.db')
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS users
-                  (username TEXT PRIMARY KEY, password TEXT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, quantity INTEGER)''')
+conn.commit()
 
-def register():
-    username = input("Введіть своє ім'я: ")
 
-    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-    existing_user = cursor.fetchone()
-    if existing_user:
-        print("Користувач з таким ім'ям вже існує. Спробуйте інше ім'я.")
-        return
+def add_product():
+    name = input("Введіть назву товару: ")
+    price = float(input("Введіть ціну товару: "))
+    quantity = int(input("Введіть кількість товару: "))
 
-    while True:
-        password = input("Ваш пароль: ")
-        confirm_password = input("Підтвердіть ваш пароль: ")
-        if password == confirm_password:
-            cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
-            conn.commit()
-            print("Ви успішно зареєструвалися!")
-            return username, password
-        else:
-            print("Пароль неправильний. Повторіть спробу.")
+    cursor.execute("INSERT INTO Products (name, price, quantity) VALUES (?, ?, ?)", (name, price, quantity))
+    conn.commit()
+    print("Товар успішно додано!")
 
-def login():
-    username = input("Введіть своє ім'я: ")
-    password = input("Введіть ваш пароль: ")
-    cursor.execute("SELECT password FROM users WHERE username=?", (username,))
-    result = cursor.fetchone()
-    if result and result[0] == password:
-        print("Все добре! Ви увійшли в обліковий запис.")
-        change_password = input("Бажаєте змінити пароль? (Так/Ні): ")
-        if change_password.lower() == "так":
-            old_password = input("Введіть ваш поточний пароль: ")
-            if old_password == password:
-                new_password = input("Введіть новий пароль: ")
-                cursor.execute("UPDATE users SET password=? WHERE username=?", (new_password, username))
-                conn.commit()
-                print("Пароль успішно змінено.")
-            else:
-                print("Неправильний поточний пароль.")
+
+def delete_product():
+    product_id = int(input("Введіть ідентифікатор товару, який потрібно видалити: "))
+
+    cursor.execute("SELECT name FROM Products WHERE id=?", (product_id,))
+    product_name = cursor.fetchone()
+
+    if product_name is None:
+        print("Товар з введеним ідентифікатором не знайдено.")
     else:
-        print("Неправильне ім'я користувача або пароль.")
+        product_name = product_name[0]
+        print("Ви збираєтесь видалити:{}".format(product_name))
+        confirm = input("Ви впевнені, що хочете видалити цей товар? (Так/Ні): ")
+
+        if confirm.lower() == "так":
+            cursor.execute("DELETE FROM Products WHERE id=?", (product_id,))
+            conn.commit()
+            print("Товар '{}' успішно видалено!".format(product_name))
+        else:
+            print("Видалення товару скасовано.")
+
+
+def edit_product():
+    product_id = int(input("Введіть ідентифікатор товару, який потрібно редагувати: "))
+    new_price = float(input("Введіть нову ціну товару: "))
+    new_quantity = int(input("Введіть нову кількість товару: "))
+
+    cursor.execute("UPDATE Products SET price=?, quantity=? WHERE id=?", (new_price, new_quantity, product_id))
+    conn.commit()
+    print("Інформацію про товар успішно оновлено!")
+
+
+def view_products():
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+
+    if len(products) == 0:
+        print("Немає жодного товару у базі даних.")
+    else:
+        for product in products:
+            print("Id:{}, Назва:{}, ціна{}, кількість{}".format(product[0], product[1], product[2], product[3] ))
+
 
 while True:
-    print("1. Зареєструватися")
-    print("2. Увійти")
-    print("3. Вийти")
-    choice = input("Виберіть пункт: ")
+    print("\nМеню:")
+    print("1. Добавити товар")
+    print("2. Видалити товар")
+    print("3. Редагувати товар")
+    print("4. Переглянути список товарів")
+    print("5. Вийти")
+
+    choice = input("Виберіть дію: ")
 
     if choice == "1":
-        register()
+        add_product()
     elif choice == "2":
-        login()
+        delete_product()
     elif choice == "3":
+        edit_product()
+    elif choice == "4":
+        view_products()
+    elif choice == "5":
         break
     else:
-        print("Неправильний вибір!")
+        print("Невірний вибір. Спробуйте ще раз.")
 
 conn.close()
